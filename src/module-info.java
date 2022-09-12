2db386077c1cb421291847e8fc20f5525befc2e3
tedomi2705 <hongducthbk123@gmail.com>
2022-09-12 21:03:33 +0700

    Convert to module project Fix: Error: JavaFX runtime components are missing, and are required to run this application

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module bomberman {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires java.desktop;
+    requires transitive javafx.graphics;
+
+    exports uet.oop.bomberman;
+}
