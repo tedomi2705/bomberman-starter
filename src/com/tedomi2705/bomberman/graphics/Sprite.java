c047150d2875c64f9e8ec98a3222ca03f0c47949
tedomi2705 <hongducthbk123@gmail.com>
2022-10-18 06:54:18 +0700

    Set SCALED_SIZE to 3x DEFAULT_SIZE

--- src/com/tedomi2705/bomberman/graphics/Sprite.java
+++ src/com/tedomi2705/bomberman/graphics/Sprite.java
@@ -9,7 +9,7 @@ public class Sprite {
 	
 	public static final int ANIMATING_CYCLE = 20;
 	public static final int DEFAULT_SIZE = 16;
-	public static final int SCALED_SIZE = DEFAULT_SIZE * 2;
+	public static final int SCALED_SIZE = DEFAULT_SIZE * 3;
     private static final int TRANSPARENT_COLOR = 0xffff00ff;
 	public final int SIZE;
 	private int _x, _y;
